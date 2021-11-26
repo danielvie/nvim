77502912413ba6448bb18ba907f77583f3a31a80
daniel <danielvie@gmail.com>
2021-11-26 20:54:33 +0100

    adicionando BUNDLE com arquivos no repositorio

--- bundle/clone_plugins.cmd
+++ bundle/clone_plugins.cmd
@@ -1,21 +1,21 @@
 
 git clone --depth 1 https://github.com/tpope/vim-commentary
 
-@REM git clone https://github.com/terryma/vim-multiple-cursors
-@REM git clone https://github.com/airblade/vim-gitgutter
-@REM git clone https://github.com/ctrlpvim/ctrlp.vim
-@REM git clone https://github.com/frazrepo/vim-rainbow
-@REM git clone https://github.com/itchyny/lightline.vim
-@REM git clone https://github.com/jiangmiao/auto-pairs.git
-@REM git clone https://github.com/preservim/nerdtree
-@REM git clone https://github.com/tpope/vim-fugitive
-@REM git clone https://github.com/tpope/vim-surround
-@REM git clone https://github.com/wellle/targets.vim
-@REM git clone https://github.com/easymotion/vim-easymotion
-@REM git clone https://github.com/justinmk/vim-sneak
-@REM git clone https://github.com/machakann/vim-highlightedyank
-@REM git clone https://github.com/andymass/vim-matchup
-@REM git clone https://github.com/airblade/vim-rooter
-@REM git clone https://github.com/sheerun/vim-polyglot
-@REM git clone https://github.com/honza/vim-snippets
-@REM git clone https://github.com/Yggdroot/indentLine
+git clone --depth 1 https://github.com/terryma/vim-multiple-cursors
+git clone --depth 1 https://github.com/airblade/vim-gitgutter
+git clone --depth 1 https://github.com/ctrlpvim/ctrlp.vim
+git clone --depth 1 https://github.com/frazrepo/vim-rainbow
+git clone --depth 1 https://github.com/itchyny/lightline.vim
+git clone --depth 1 https://github.com/jiangmiao/auto-pairs.git
+git clone --depth 1 https://github.com/preservim/nerdtree
+git clone --depth 1 https://github.com/tpope/vim-fugitive
+git clone --depth 1 https://github.com/tpope/vim-surround
+git clone --depth 1 https://github.com/wellle/targets.vim
+git clone --depth 1 https://github.com/easymotion/vim-easymotion
+git clone --depth 1 https://github.com/justinmk/vim-sneak
+git clone --depth 1 https://github.com/machakann/vim-highlightedyank
+git clone --depth 1 https://github.com/andymass/vim-matchup
+git clone --depth 1 https://github.com/airblade/vim-rooter
+git clone --depth 1 https://github.com/sheerun/vim-polyglot
+git clone --depth 1 https://github.com/honza/vim-snippets
+git clone --depth 1 https://github.com/Yggdroot/indentLine
