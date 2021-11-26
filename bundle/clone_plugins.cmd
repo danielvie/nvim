f4e78a9ad8b85f991b2e42fc4a01c00450149522
daniel <danielvie@gmail.com>
2021-11-26 20:50:02 +0100

    delete bundle

--- bundle/clone_plugins.cmd
+++ bundle/clone_plugins.cmd
@@ -1,20 +1,21 @@
 
-git clone https://github.com/terryma/vim-multiple-cursors
-git clone https://github.com/airblade/vim-gitgutter
-git clone https://github.com/ctrlpvim/ctrlp.vim
-git clone https://github.com/frazrepo/vim-rainbow
-git clone https://github.com/itchyny/lightline.vim
-git clone https://github.com/jiangmiao/auto-pairs.git
-git clone https://github.com/preservim/nerdtree
-git clone https://github.com/tpope/vim-commentary
-git clone https://github.com/tpope/vim-fugitive
-git clone https://github.com/tpope/vim-surround
-git clone https://github.com/wellle/targets.vim
-git clone https://github.com/easymotion/vim-easymotion
-git clone https://github.com/justinmk/vim-sneak
-git clone https://github.com/machakann/vim-highlightedyank
-git clone https://github.com/andymass/vim-matchup
-git clone https://github.com/airblade/vim-rooter
-git clone https://github.com/sheerun/vim-polyglot
-git clone https://github.com/honza/vim-snippets
-git clone https://github.com/Yggdroot/indentLine
+git clone --depth 1 https://github.com/tpope/vim-commentary
+
+@REM git clone https://github.com/terryma/vim-multiple-cursors
+@REM git clone https://github.com/airblade/vim-gitgutter
+@REM git clone https://github.com/ctrlpvim/ctrlp.vim
+@REM git clone https://github.com/frazrepo/vim-rainbow
+@REM git clone https://github.com/itchyny/lightline.vim
+@REM git clone https://github.com/jiangmiao/auto-pairs.git
+@REM git clone https://github.com/preservim/nerdtree
+@REM git clone https://github.com/tpope/vim-fugitive
+@REM git clone https://github.com/tpope/vim-surround
+@REM git clone https://github.com/wellle/targets.vim
+@REM git clone https://github.com/easymotion/vim-easymotion
+@REM git clone https://github.com/justinmk/vim-sneak
+@REM git clone https://github.com/machakann/vim-highlightedyank
+@REM git clone https://github.com/andymass/vim-matchup
+@REM git clone https://github.com/airblade/vim-rooter
+@REM git clone https://github.com/sheerun/vim-polyglot
+@REM git clone https://github.com/honza/vim-snippets
+@REM git clone https://github.com/Yggdroot/indentLine
