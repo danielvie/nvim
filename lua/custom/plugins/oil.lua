04c0ca9e22e4407e27d9327b9bfa132039d94bf9
Daniel Vieira <danielvie@gmail.com>
2024-06-12 21:28:42 -0300

    update Oil.lua

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -4,6 +4,8 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       require('oil').setup {
+        default_file_explorer = true,
+        skip_confirm_for_simple_edits = true,
         columns = { 'icon' },
         keymaps = {
           ['<C-h>'] = false,
@@ -11,6 +13,13 @@ return {
         },
         view_options = {
           show_hidden = true,
+          natural_order = true,
+          -- is_always_hidden = function(name, _)
+          --   return name == '..' or name == '.git'
+          -- end,
+        },
+        win_options = {
+          wrap = true,
         },
       }
 
