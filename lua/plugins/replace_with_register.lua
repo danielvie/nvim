7414bab5febfd217e0ee931f939d9a61e112d3c6
Daniel Vieira <danielvie@gmail.com>
2025-05-05 01:43:59 -0300

    update mapping for replace with register

--- lua/plugins/replace_with_register.lua
+++ lua/plugins/replace_with_register.lua
@@ -1,5 +1,14 @@
 return {
   'inkarkat/vim-ReplaceWithRegister',
+  init = function()
+    -- Prevent default gr mappings
+    vim.g.ReplaceWithRegister_no_mappings = 1
+  end,
 
-  config = function() end,
+  config = function()
+    -- Define custom mappings
+    vim.keymap.set("n", "<leader>r", "<Plug>ReplaceWithRegisterOperator", { noremap = true })
+    vim.keymap.set("v", "<leader>r", "<Plug>ReplaceWithRegisterVisual", { noremap = true })
+    vim.keymap.set("n", "<leader>rr", "<Plug>ReplaceWithRegisterLine", { noremap = true })
+  end,
 }
