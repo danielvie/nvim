baae6aee31593f6f969df27739137fb67ba91ab6
Daniel Vieira <danielvie@gmail.com>
2025-05-10 17:54:06 -0300

    add conform.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,21 +1,32 @@
+-- oil
+vim.keymap.set("n", "-", "<cmd>Oil --float<cr>", { desc = "Open file explorer in Oil" })
+
+-- diagnostics
+vim.keymap.set("n", "gl", function()
+  vim.diagnostic.open_float()
+end, { desc = "Open Diagnostics in Float" })
+
+-- conform (formatter)
+vim.keymap.set("n", "<leader>cf", function()
+  require("conform").format({
+    lsp_format = "fallback",
+  })
+end, { desc = "Format current file" })
+
 -- facilities
-vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", {desc = "Save"})
-vim.keymap.set("n", "<a-x>", "<cmd>bd<cr>", {desc = "Delete Buffer"})
-vim.keymap.set("n", "<leader>0", "<cmd>cd %:p:h<cr>", {desc = "Set Root Folder"})
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
+vim.keymap.set("n", "<a-x>", "<cmd>bd<cr>", { desc = "Delete Buffer" })
+vim.keymap.set("n", "<leader>0", "<cmd>cd %:p:h<cr>", { desc = "Set Root Folder" })
 
-vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", {desc = "Clear search highlight"})
-
+vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
 
 vim.keymap.set("n", "<s-h>", "<cmd>bp<cr>", { desc = "Prev Buffer" })
 vim.keymap.set("n", "<s-l>", "<cmd>bn<cr>", { desc = "Next Buffer" })
 
 vim.keymap.set("n", "<leader><leader>t", "ggVG", { desc = "select [A]ll" })
 
-vim.keymap.set("n", "<leader><leader>x", "<cmd>:source %<cr>", {desc = "Source file"})
+vim.keymap.set("n", "<leader><leader>x", "<cmd>:source %<cr>", { desc = "Source file" })
 vim.keymap.set("n", "<s-r>", "za", { desc = "Toggle Fold" })
-
--- diagnostics
-vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, {desc = "Open Diagnostics in Float"})
 
 -- macros
 vim.keymap.set("n", "<s-q>", "@q", { desc = "run macro @q" })
@@ -26,9 +37,6 @@ vim.keymap.set("n", "<s-e>", "@e", { desc = "run macro @e" })
 local terminal_command = "powershell"
 vim.keymap.set("n", "<s-t>", "<cmd>.!" .. terminal_command .. "<cr>", { desc = "run line as cmd" })
 vim.keymap.set("v", "<s-t>", ":'<,'>!" .. terminal_command .. "<cr>", { desc = "run line as cmd" })
-
--- oil
-vim.keymap.set("n", "-", "<cmd>Oil --float<cr>", {desc = "Open file explorer in Oil"})
 
 -- operators
 vim.keymap.set("o", "<s-l>", "$", { desc = "operators to end of line" })
