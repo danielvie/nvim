f44f193ed1b185c791237805cf67343f7b2e2287
Daniel Vieira <danielvie@gmail.com>
2025-05-12 00:27:01 -0300

    add undotree and change leader on repl_with_reg

--- lua/plugins/replace_with_register.lua
+++ lua/plugins/replace_with_register.lua
@@ -7,8 +7,7 @@ return {
 
     config = function()
         -- Define custom mappings
-        vim.keymap.set("n", "<leader>r", "<Plug>ReplaceWithRegisterOperator", { noremap = true })
-        vim.keymap.set("v", "<leader>r", "<Plug>ReplaceWithRegisterVisual", { noremap = true })
-        vim.keymap.set("n", "<leader>rr", "<Plug>ReplaceWithRegisterLine", { noremap = true })
+        vim.keymap.set("n", "<localleader>gr", "<Plug>ReplaceWithRegisterOperator", { noremap = true })
+        vim.keymap.set("v", "<localleader>gr", "<Plug>ReplaceWithRegisterVisual", { noremap = true })
     end,
 }
---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        "mbbill/undotree",
+        config = function()
+            vim.keymap.set("n", "<localleader>u", vim.cmd.UndotreeToggle)
+        end,
+    },
+}
